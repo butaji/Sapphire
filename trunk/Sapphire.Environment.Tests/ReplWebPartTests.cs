7053b46a88d951753a2925fbd368145f2ffcbc30
Vitaly Baum <vbaum@live.com>
2009-10-24 13:32:03 +0400

    missing tests

--- trunk/Sapphire.Environment.Tests/ReplWebPartTests.cs
+++ trunk/Sapphire.Environment.Tests/ReplWebPartTests.cs
@@ -1,5 +1,7 @@
 using MbUnit.Framework;
+using Microsoft.SharePoint;
 using Sapphire.Environment.Runtime;
+using TypeMock.ArrangeActAssert;
 
 namespace Sapphire.Environment.Tests
 {
@@ -21,6 +23,19 @@ namespace Sapphire.Environment.Tests
       python.Execute("Console.WriteLine('hello world')");
       var x = (Console)python.GetVar("Console");
       Assert.AreEqual(x.Message, "hello world\r\n");
+    }
+
+    [Test]
+    private void repl_webpart_should_fire_python_scripts()
+    {
+      SPWeb web = Isolate.Fake.Instance<SPWeb>();
+      SPContext context = Isolate.Fake.Instance<SPContext>();
+      Isolate.WhenCalled(() => web.Title).WillReturn("Fake Title");
+      Isolate.WhenCalled(() => context.Web).WillReturn(web);
+      var language = new LanguagesFactory().Create("Python");
+      var sharepointContext = new SharePointContext(context, language, new Console());
+      language.Execute("Console.Write(__web__.Title)");
+      Assert.AreEqual(sharepointContext.Message, web.Title);
     }
   }
 }
